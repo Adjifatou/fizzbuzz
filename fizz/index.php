0aae15d970bd99d7376207fc136014306fc75d53
Adjifatou <43034624+Adjifatou@users.noreply.github.com>
2018-12-12 10:53:33 +0000

    fizzbuzz

---
+++ fizz/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>fizz buzz</title>
+</head>
+<body>
+	<?php
+		
+		$fizz = 3;
+		$buzz = 5;
+		for($i = 1 ; $i <= 100 ; $i++) 
+		{
+    	if($i % $fizz == 0 && $i % $buzz != 0) 
+    	{
+        echo "fizz<br>";
+    	}
+    	else if($i % $fizz != 0 && $i % $buzz == 0) 
+    	{
+        echo "buzz<br>";
+    	}
+    	else if($i % $fizz == 0 && $i % $buzz == 0) 
+    	{
+        echo "fizz-buzz<br>";
+    	}
+    	else {
+        echo $i . "<br>";
+    	}
+		}
+	?>
+</body>
+</html>
